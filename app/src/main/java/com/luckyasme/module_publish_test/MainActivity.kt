45e53a423b7c757e3a019644789b9c0cd9ce69c7
luckyasme <807254037@qq.com>
2023-08-09 18:08:43 +0800

    .

--- app/src/main/java/com/luckyasme/module_publish_test/MainActivity.kt
+++ app/src/main/java/com/luckyasme/module_publish_test/MainActivity.kt
@@ -2,12 +2,10 @@ package com.luckyasme.module_publish_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.luckyasme.module_publish.TestA
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TestA.echo()
     }
 }
